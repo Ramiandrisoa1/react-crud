d014115729712cf4ce86eaeff0b0f0d5113e0917
Rak <hfr@esokia.com>
2022-06-21 09:54:07 +0300

    feat modal

--- src/App.jsx
+++ src/App.jsx
@@ -2,21 +2,51 @@ import React, { Component } from 'react';
 import './App.css';
 import Table from './component/table/table';
 import Form from './component/form/form';
+import Modal from 'react-modal';
 
+Modal.setAppElement('#root');
 class App extends Component {
   state = {
     characters: [],
+    modalIsOpen: false,
   };
+
+  openModal = () => {
+    this.setState({ modalIsOpen: true });
+  };
+
+  closeModal = () => {
+    this.setState({ modalIsOpen: false });
+  };
+
   render() {
     const { characters } = this.state;
 
     return (
       <div className='container'>
+        <button onClick={this.openModal}>Ajout</button>
         <Table
           characterData={characters}
           removeCharacter={this.removeCharacter}
         />
-        <Form handleSubmit={this.handleSubmit} />
+        <Modal
+          isOpen={this.state.modalIsOpen}
+          onRequestClose={this.closeModal}
+          overlayClassName={{
+            base: 'overlay-base',
+            afterOpen: 'overlay-after',
+            beforeClose: 'overlay-before',
+          }}
+          className={{
+            base: 'content-base',
+            afterOpen: 'content-after',
+            beforeClose: 'content-before',
+          }}
+          closeTimeoutMS={500}
+        >
+          <button onClick={this.closeModal}>close</button>
+          <Form handleSubmit={this.handleSubmit} closeModal={this.closeModal} />
+        </Modal>
       </div>
     );
   }
